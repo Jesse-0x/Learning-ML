d10cd2720dcdcdf8f4c58303d0c959b5d38c80e8
Jesse0x <jgong@whschool.org>
2022-07-24 19:23:01 -0400

    Gan is too hard... Gonna learn ML step by step

--- main.py
+++ main.py
@@ -1,15 +1,133 @@
-import tensorflow as tf
+MIDI_SONG_DEFAULT = './gansynth/midi/bach.mid'
+MIDI_RIFF_DEFAULT = '/content/gansynth/midi/riff-default.mid'
+
+import os
+import librosa
+from magenta.models.nsynth.utils import load_audio
+from magenta.models.gansynth.lib import flags as lib_flags
+from magenta.models.gansynth.lib import generate_util as gu
+from magenta.models.gansynth.lib import model as lib_model
+from magenta.models.gansynth.lib import util
+import matplotlib.pyplot as plt
+import note_seq
+from note_seq.notebook_utils import colab_play as play
 import numpy as np
-import keras
+import tensorflow.compat.v1 as tf
 
+tf.disable_v2_behavior()
 
-model = keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])])
-model.compile(optimizer='sgd', loss='mean_squared_error')
-x = [-15.0, -14.0, -13.0, -12.0, -11.0, -10.0, -9.0, -8.0, -7.0, -6.0, -5.0, -4.0, -3.0, -2.0, -1.0, 0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0]
-y = [-31.0, -29.0, -27.0, -25.0, -23.0, -21.0, -19.0, -17.0, -15.0, -13.0, -11.0, -9.0, -7.0, -5.0, -3.0, -1.0, 1.0, 3.0, 5.0, 7.0, 9.0, 11.0, 13.0, 15.0, 17.0, 19.0, 21.0, 23.0, 25.0, 27.0]
+# File IO
+download = files.download
 
-xs = np.array(x, dtype=float)
-ys = np.array(y, dtype=float)
+def upload():
+  '''Upload a .wav file.'''
+  filemap = files.upload()
+  file_list = []
+  for key, value in filemap.items():
+    fname = os.path.join('/content/gansynth/midi', key)
+    with open(fname, 'wb') as f:
+      f.write(value)
+      print('Writing {}'.format(fname))
+    file_list.append(fname)
+  return file_list
 
-model.fit(xs, ys, epochs=10000)
-model.save('my_model.h5')
+# GLOBALS
+CKPT_DIR = 'gs://magentadata/models/gansynth/acoustic_only'
+output_dir = '/content/gansynth/samples'
+BATCH_SIZE = 16
+SR = 16000
+
+# Make an output directory if it doesn't exist
+OUTPUT_DIR = util.expand_path(output_dir)
+if not tf.gfile.Exists(OUTPUT_DIR):
+  tf.gfile.MakeDirs(OUTPUT_DIR)
+
+# Load the model
+tf.reset_default_graph()
+flags = lib_flags.Flags({
+    'batch_size_schedule': [BATCH_SIZE],
+    'tfds_data_dir': "gs://tfds-data/datasets",
+})
+model = lib_model.Model.load_from_path(CKPT_DIR, flags)
+
+# Helper functions
+def load_midi(midi_path, min_pitch=36, max_pitch=84):
+  """Load midi as a notesequence."""
+  midi_path = util.expand_path(midi_path)
+  ns = note_seq.midi_file_to_sequence_proto(midi_path)
+  pitches = np.array([n.pitch for n in ns.notes])
+  velocities = np.array([n.velocity for n in ns.notes])
+  start_times = np.array([n.start_time for n in ns.notes])
+  end_times = np.array([n.end_time for n in ns.notes])
+  valid = np.logical_and(pitches >= min_pitch, pitches <= max_pitch)
+  notes = {'pitches': pitches[valid],
+           'velocities': velocities[valid],
+           'start_times': start_times[valid],
+           'end_times': end_times[valid]}
+  return ns, notes
+
+def get_envelope(t_note_length, t_attack=0.010, t_release=0.3, sr=16000):
+  """Create an attack sustain release amplitude envelope."""
+  t_note_length = min(t_note_length, 3.0)
+  i_attack = int(sr * t_attack)
+  i_sustain = int(sr * t_note_length)
+  i_release = int(sr * t_release)
+  i_tot = i_sustain + i_release  # attack envelope doesn't add to sound length
+  envelope = np.ones(i_tot)
+  # Linear attack
+  envelope[:i_attack] = np.linspace(0.0, 1.0, i_attack)
+  # Linear release
+  envelope[i_sustain:i_tot] = np.linspace(1.0, 0.0, i_release)
+  return envelope
+
+def combine_notes(audio_notes, start_times, end_times, velocities, sr=16000):
+  """Combine audio from multiple notes into a single audio clip.
+
+  Args:
+    audio_notes: Array of audio [n_notes, audio_samples].
+    start_times: Array of note starts in seconds [n_notes].
+    end_times: Array of note ends in seconds [n_notes].
+    sr: Integer, sample rate.
+
+  Returns:
+    audio_clip: Array of combined audio clip [audio_samples]
+  """
+  n_notes = len(audio_notes)
+  clip_length = end_times.max() + 3.0
+  audio_clip = np.zeros(int(clip_length) * sr)
+
+  for t_start, t_end, vel, i in zip(start_times, end_times, velocities, range(n_notes)):
+    # Generate an amplitude envelope
+    t_note_length = t_end - t_start
+    envelope = get_envelope(t_note_length)
+    length = len(envelope)
+    audio_note = audio_notes[i, :length] * envelope
+    # Normalize
+    audio_note /= audio_note.max()
+    audio_note *= (vel / 127.0)
+    # Add to clip buffer
+    clip_start = int(t_start * sr)
+    clip_end = clip_start + length
+    audio_clip[clip_start:clip_end] += audio_note
+
+  # Normalize
+  audio_clip /= audio_clip.max()
+  audio_clip /= 2.0
+  return audio_clip
+
+# Plotting tools
+def specplot(audio_clip):
+  p_min = np.min(36)
+  p_max = np.max(84)
+  f_min = librosa.midi_to_hz(p_min)
+  f_max = 2 * librosa.midi_to_hz(p_max)
+  octaves = int(np.ceil(np.log2(f_max) - np.log2(f_min)))
+  bins_per_octave = 36
+  n_bins = int(bins_per_octave * octaves)
+  C = librosa.cqt(audio_clip, sr=SR, hop_length=2048, fmin=f_min, n_bins=n_bins, bins_per_octave=bins_per_octave)
+  power = 10 * np.log10(np.abs(C)**2 + 1e-6)
+  plt.matshow(power[::-1, 2:-2], aspect='auto', cmap=plt.cm.magma)
+  plt.yticks([])
+  plt.xticks([])
+
+print('And...... Done!')
