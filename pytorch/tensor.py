b498b301091685d45a8db6f4bebb0583e3f5bb2a
Jesse-0x <jgong@whschool.org>
2023-04-26 23:15:23 -0700

    :

---
+++ pytorch/tensor.py
@@ -0,0 +1,21 @@
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+
+# draw a graph of tensor
+def draw_tensor_picture(tensor):
+    # draw as a gray scale picture
+    plt.imshow(tensor, cmap='gray')
+    plt.show()
+
+def flush_tensor_picture(tensor):
+    # flush the picture
+    plt.imshow(tensor, cmap='gray')
+    plt.draw()
+    plt.pause(0.001)
+    plt.clf()
+
+while 1:
+    my_tensor = torch.rand(100, 100)
+    # draw_tensor_picture(my_tensor)
+    flush_tensor_picture(my_tensor)
